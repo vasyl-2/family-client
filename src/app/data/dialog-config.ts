b9897f0af2389e3ba22da38b62677018da66cada
Vasiliy <dvv03122014@gmail.com>
2023-06-15 17:57:55 +0300

    test git

--- src/app/data/dialog-config.ts
+++ src/app/data/dialog-config.ts
@@ -2,6 +2,6 @@ import {MatDialogConfig} from "@angular/material/dialog";
 
 export const DIALOG_CONFIG: MatDialogConfig = {
   width: '600px',
-  height: '800px',
+  height: '801px',
   backdropClass: 'dialog-default'
 }
