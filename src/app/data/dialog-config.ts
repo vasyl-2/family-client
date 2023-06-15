1b3b7b7e514bbd52421ac99d4158465ed8920872
Vasiliy <dvv03122014@gmail.com>
2023-06-15 17:41:14 +0300

    feat(upload): get all photos and its state

--- src/app/data/dialog-config.ts
+++ src/app/data/dialog-config.ts
@@ -3,5 +3,5 @@ import {MatDialogConfig} from "@angular/material/dialog";
 export const DIALOG_CONFIG: MatDialogConfig = {
   width: '600px',
   height: '800px',
-  backdropClass: 'dialog-default',
+  backdropClass: 'dialog-default'
 }
