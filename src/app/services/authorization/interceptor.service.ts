2daef4c145df8c712ed1ce9fafb0e5ae6e8b7b15
Vasiliy <dvv03122014@gmail.com>
2023-10-08 17:41:11 +0300

    auth-implemented

--- src/app/services/authorization/interceptor.service.ts
+++ src/app/services/authorization/interceptor.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs";
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +17,7 @@ export class InterceptorService implements HttpInterceptor {
       const authReq = req.clone({
         headers: req.headers.set(
           'authorization',
-          token as string
+          `Bearer  ${token as string}`
         ),
       });
 
