5f74d6d39d0a56602986b5e167f36094ea85150a
Vasiliy <dvv03122014@gmail.com>
2024-01-07 19:13:35 +0200

    hierarchy photos
    hierarchy photos

--- src/app/shared/components/photos-list/photos-list.component.ts
+++ src/app/shared/components/photos-list/photos-list.component.ts
@@ -1,7 +1,8 @@
 import {ChangeDetectionStrategy, Component, OnDestroy, OnInit} from '@angular/core';
-import {ActivatedRoute, ParamMap} from "@angular/router";
-import {BehaviorSubject, combineLatest, Observable, Subscription} from "rxjs";
+import {ActivatedRoute} from "@angular/router";
+import {BehaviorSubject, Observable, Subscription} from "rxjs";
 import {select, Store} from "@ngrx/store";
+import {map, shareReplay, withLatestFrom} from "rxjs/operators";
 
 import {GalleryState} from "../../../store/reducer";
 import {receivePhotos} from "../../../store/action";
@@ -9,7 +10,6 @@ import {chaptersHierarchySelector, photosSelector} from "../../../store/selector
 import {Photo} from "../../../models/photo";
 import {environment} from "../../../../environments/environment";
 import {Chapter} from "../../../models/chapter";
-import {map, shareReplay, startWith, withLatestFrom} from "rxjs/operators";
 
 @Component({
   selector: 'app-photos-list',
@@ -26,8 +26,6 @@ export class PhotosListComponent implements OnInit, OnDestroy{
   private readonly selectedIdSubject = new BehaviorSubject<string | undefined>('');
   private readonly selectedId$ = this.selectedIdSubject.asObservable().pipe(shareReplay(1));
 
-  // chapter$!: Observable<string>;
-
   private sub = new Subscription();
 
   constructor(
@@ -40,35 +38,10 @@ export class PhotosListComponent implements OnInit, OnDestroy{
     this.subscribeToRoute();
     this.photos$ = this.store.pipe(select(photosSelector));
 
-    // this.subChapters$ = this.store.pipe(
-    //   select(chaptersHierarchySelector),
-    //   map((chapters: Chapter[]) => {
-    //     return chapters.find((c: Chapter) => c._id === this.route.snapshot.params['chapter'])!
-    //   }),
-    //   shareReplay(1)
-    // );
-
-
-    // this.subChapters$ = combineLatest([
-    //   this.store.pipe(select(chaptersHierarchySelector)),
-    //   this.selectedId$
-    // ]).pipe(
-    //   map(([chapters, id]: [Chapter[], string | undefined]) => {
-    //     if (id) {
-    //       return chapters.find((c: Chapter) => c._id === id)!;
-    //     } else {
-    //       return chapters.find((c: Chapter) => c._id === this.route.snapshot.params['chapter'])!
-    //     }
-    //   }),
-    // );
-
     this.subChapter$ = this.selectedId$.pipe(
       withLatestFrom(this.store.pipe(select(chaptersHierarchySelector))),
       map(([id, chapters]: [string | undefined, Chapter[]]) => {
         if (!!id) {
-          console.log('1_______', id);
-          console.log('1_______!!!!!!!', chapters);
-
           const chapter = chapters.find((c: Chapter) => c.children?.find((c: Chapter) => c._id === id))!
 
           if (chapter && chapter.children) {
@@ -77,34 +50,11 @@ export class PhotosListComponent implements OnInit, OnDestroy{
             this.store.dispatch(receivePhotos({ chapter: this.route.snapshot.params['chapter'] }));
             return chapters.find((c: Chapter) => c._id === this.route.snapshot.params['chapter'])!
           }
-
         } else {
-          console.log('2_______');
-
           return chapters.find((c: Chapter) => c._id === this.route.snapshot.params['chapter'])!
         }
       }),
     )
-
-    // this.chapter$ = combineLatest([
-    //   this.subChapters$,
-    //   this.selectedId$
-    // ]).pipe(
-    //   map(([subChapters, id]: [Chapter, string | undefined]) => {
-    //     if (id) {
-    //       const chapter = subChapters.children!.find((ch: Chapter) => ch._id === id);
-    //       return chapter!.nameForUI!
-    //     } else {
-    //       if (subChapters._id === this.route.snapshot.params['chapter']) {
-    //         return subChapters.nameForUI!;
-    //       } else {
-    //         const chapter = subChapters.children!.find((ch: Chapter) => ch._id === this.route.snapshot.params['chapter']);
-    //         return chapter!.nameForUI!
-    //       }
-    //     }
-    //   })
-    // )
-
 
     this.subChapter$.subscribe((c) => {
       console.log('SUB___CHAPTERS____________', c);
